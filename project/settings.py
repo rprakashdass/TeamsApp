751559df904a2cc1f58ef144d48937c59f0043e4
rprakashdass <rprakashdass@gmail.com>
2024-04-01 17:33:45 +0530

    Tasks Update

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app',
     'tasks.apps.TasksConfig',
     "teams.apps.TeamsConfig",
     'django.contrib.admin',
--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("teams.urls")),
+    path("", include("app.urls")),
+    path("teams/", include("teams.urls")),
     path("tasks/", include("tasks.urls")),
     path("admin/", admin.site.urls),
 ]
